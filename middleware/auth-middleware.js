bdf23922556b6459721e0088f9fd6a983e3dbfed
mayank0801mishra <mayank08mrinal04@gmail.com>
2023-03-29 14:55:26 +0530

    Middleware is ready till

--- middleware/auth-middleware.js
+++ middleware/auth-middleware.js
@@ -4,6 +4,27 @@ const UserModel = require('../models/user')
 const checkUserAuth = async(req,res,next) =>{
     const token = req.headers.authorization.split(' ')[1];
 
+    if(!token) return res.status(401).send('Access Denied !');
+
+    console.log("ourtoken",token);
+
+    try{
+        const UserID = await jwt.verify(token,process.env.JWT_SECRET_KEY);
+        console.log("Your",UserID)
+
+        if(!UserID){
+            return res.send("test not done")
+        }
+
+        req.user = UserID;
+        next();
+
+    }
+    catch(error){
+     res.status(400).send('Invalid token !');
+
+    }
+
         
 
 
